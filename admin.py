c27bba5c36b450b215ea50b9e477d643befa1752
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-04-26 15:19:35 -0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,13 @@
+# models here.
+from django.contrib import admin
+from .models import Partido_politico
+from .models import Candidato
+from .models import Votante
+from .models import Voto
+
+#register models here.
+admin.site.register(Partido_politico)
+admin.site.register(Candidato)
+admin.site.register(Votante)
+admin.site.register(Voto)
 
