b6c9403698c1ad00a612738e289d61638c31f869
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-05-03 13:14:41 -0500

    Update views.py

--- views.py
+++ views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Acá se Crea la visa del archivo your views here.
 
-def index(request):
+def index(solicitud):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def Detalles(solicitud, pregunta_id):
+    return HttpResponse("Esta es su opcion %s." % pregunta_id)
+def Resultados(solicitud, pregunta_id):
+    response = "Esta este es el resultado %s."
+    return HttpResponse(response % pregunta_id)
+
+def Voto( solicitud, pregunta_id):
+    return HttpResponse( "tu Voto fue %s." % pregunta_id)
