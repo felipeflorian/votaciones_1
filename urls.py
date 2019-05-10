da3a613ea09298ef28c9c208df4db7cf89e5b073
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-05-10 12:23:58 -0500

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex: /elecciones/
+    path('', views.index, name='index'),
+    #ex : /elecciones/5/
+    path('<int:pregunta_id>/', views.Detalles, name='Detalles'),
+    # ex: /polls/5/Resultados/
+    path('<int:pregunta_id>/Resultados/', views.ResultsView.as_view(), name='Resultados'),
+    # ex: /polls/5/vote/
+    path('<int:pregunta_id>/Voto/', views.Voto, name='Voto'),
+]
