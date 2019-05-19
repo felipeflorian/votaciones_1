399e612ee3b3413a91577467130d353a0d22ca32
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2019-05-19 16:54:10 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #ex : /elecciones/5/
     path('<int:pregunta_id>/', views.Detalles, name='Detalles'),
     # ex: /polls/5/Resultados/
-    path('<int:pregunta_id>/Resultados/', views.ResultsView.as_view(), name='Resultados'),
+    path('<int:pregunta_id>/Resultados/', views.Resultados, name='Resultados'),
     # ex: /polls/5/vote/
     path('<int:pregunta_id>/Voto/', views.Voto, name='Voto'),
 ]
